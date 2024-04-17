7fc710a20758276a471abf5792251e6629f07396
1202002anada <kd1355752@st.kobedenshi.ac.jp>
2024-04-17 10:30:50 +0900

    絶対に消すコード
    １

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,9 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	//必ず消すこと
+	m_test = 1;
+
 	return true;
 }
 
